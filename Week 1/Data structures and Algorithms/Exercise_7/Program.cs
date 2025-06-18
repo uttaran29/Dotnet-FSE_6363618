1ce74ee95325f9bd43bc1695d2794e325fa723a4
Uttaran Sarkar <sarkaruttaran2004@gmail.com>
2025-06-18 17:15:14 +0530

    Exercise 7 completed and the solution screenshort added

---
+++ Week 1/Data structures and Algorithms/Exercise_7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    internal class Program
+    {
+
+        static double PredictFutureValue(double presentValue, double growthRate, int time)
+        {
+            if (time == 0) { 
+                return presentValue;
+            }
+
+            return PredictFutureValue(presentValue, growthRate, time-1) * (1+growthRate);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the present ammout-->");
+            double presentAmount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the annual interest rate-->");
+            double rate = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the time of investment-->");
+            int time = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Fututre value after {time} years is : {PredictFutureValue(presentAmount,rate/100,time)} .");
+        }
+    }
+}
