f858496c5451f68efd9c1482077bd5c5ff51e108
Uttaran Sarkar <sarkaruttaran2004@gmail.com>
2025-07-13 11:26:00 +0530

    Handson 2 completed

---
+++ Week4_ASP.NET Core 8.0 Web API/WebApi_Handson_2/Code/SwaggerDemoAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SwaggerDemoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/emp")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Uttaran", "Sarkar" };
+        }
+    }
+}
---
+++ Week4_ASP.NET Core 8.0 Web API/WebApi_Handson_2/Code/SwaggerDemoAPI/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.OpenApi.Models;
+
+namespace SwaggerDemoAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Swagger Demo",
+                    Version = "v1",
+                    Description = "TBD",
+                    TermsOfService = new Uri("https://example.com"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Uttaran Sarkar",
+                        Email = "uttaran@xyzmail.com",
+                        Url = new Uri("https://www.uttaran.com")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "License Terms",
+                        Url = new Uri("https://www.example.com")
+                    }
+                });
+            });
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
