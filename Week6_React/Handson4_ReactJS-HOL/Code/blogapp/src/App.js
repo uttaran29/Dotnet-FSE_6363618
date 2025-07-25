3c462ff8ff20feaa074005c265590ea1a6ebe9dd
Uttaran Sarkar <sarkaruttaran2004@gmail.com>
2025-07-25 18:45:31 +0530

    Handson 4 completed

---
+++ Week6_React/Handson4_ReactJS-HOL/Code/blogapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Posts from "../src/Posts";
+
+function App() {
+  return (
+    <div className="App">
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week6_React/Handson4_ReactJS-HOL/Code/blogapp/src/Posts.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Post from "../src/Post";
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        const postList = data.map(p => new Post(p.id, p.title, p.body));
+        this.setState({ posts: postList });
+      });
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    alert("An error occurred while rendering posts.");
+    console.error("Error caught:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
